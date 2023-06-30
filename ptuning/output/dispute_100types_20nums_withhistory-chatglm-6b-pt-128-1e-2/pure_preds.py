83a20ed20e0447ec36dc81f2151f7efeb9a2f179
han.zhang12 <windyalan@foxmail.com>
2023-06-30 11:28:55 +0800

    add pure_preds

---
+++ ptuning/output/dispute_100types_20nums_withhistory-chatglm-6b-pt-128-1e-2/pure_preds.py
@@ -0,0 +1,27 @@
+import json
+test_file = '/home/hanzhang/GLMTest/ptuning/data/dispute/dispute_test_100_20_0.json'
+file = 'generated_predictions.txt'
+preds = []
+with open(test_file,'r') as f:
+    test = json.load(f)
+res = test
+
+with open(file,'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        dic = json.loads(line)
+        i = dic['labels'].index('人工客服')
+        dic['labels'] = dic['labels'][i:]
+        preds.append(dic)
+
+for i,pred_dic in enumerate(preds):
+    for key,value in pred_dic.items():
+        if key != 'labels':
+            res[i][key] = value
+
+print('res_len:',len(res))
+print('preds_len',len(preds))
+
+#print(res)
+with open('preds.json','w') as f:
+    json.dump(res,f,indent=4,ensure_ascii=False)
